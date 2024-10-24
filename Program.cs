d3cf3ab3465be7fd69a0fc98ba31af921a1fe420
Jenny Lind <jeli2308@student.miun.se>
2024-10-24 15:02:13 +0200

    Lagt till ett enkelt användargränssnitt med en huvudmeny där alternativ 4 för tillfället visar alla rim i JSON-filen. Skapat ett flödeschema för hur applikationen ska fungera.

--- Program.cs
+++ Program.cs
@@ -11,29 +11,89 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Läser in JSON-filen som en sträng.
-        string jsonFile = File.ReadAllText("rimsamling.json");
+        // Initierar input-variabel (hanterar null-värde med ?).
+        string? userInput;
 
-        // Deserialiserar JSON-strängen till ett RimList-objekt (hanterar nullvärden med ett ?)
-        RimList? rimList = JsonSerializer.Deserialize<RimList>(jsonFile);
+        // Loopen fortsätter tills användaren väljer att avsluta.
+        do
+        {
+            // Rensar konsollen innan varje "meny" visas.
+            Console.Clear();
 
-        // Kontrollerar om värdet är null och hanterar det med ett meddelande.
-        if (rimList == null || rimList.Rimsamling == null)
-        {
-            Console.WriteLine("Tyvärr, inga rim hittades.");
-        }
-        else
-        {
-            // Loopar genom listan av rim och skriver ut varje post.
-            foreach (var item in rimList.Rimsamling)
+            // Skriver ut en "meny" för julklappsrims-applikationen.
+            Console.WriteLine("\nVÄLKOMMEN TILL JENNY'S JULKLAPPSRIM!");
+            Console.WriteLine("\nVälj ett av följande val:");
+            Console.WriteLine("\n1 - Sök rim för en specfik julklapp.");
+            Console.WriteLine("2 - Sök julklappar utifrån kategori.");
+            Console.WriteLine("3 - Spela 'Gissa julklappen'.");
+            Console.WriteLine("4 - Visa alla rim lagrade i JSON-filen.");
+            Console.WriteLine("\n0 - Avsluta appen.\n");
+
+            // Läser in användarens input.
+            userInput = Console.ReadLine();
+
+            // Olika cases körs beroende på användarens input.
+            switch (userInput)
             {
-                Console.WriteLine($"Julklapp: {item.Julklapp}");
-                Console.WriteLine($"Rim: {item.Rim}");
-                Console.WriteLine($"Kategori: {item.Kategori}");
-                
-                // Lägger en tom rad mellan varje post.
-                Console.WriteLine();
+                // Söker rim för en julklapp.
+                case "1":
+                    Console.WriteLine("\nFungerar inte att söka rim än...");
+                    break;
+
+                // Söker julklappar utifrån kategori.
+                case "2":
+                    Console.WriteLine("\nFungerar inte att söka julklappar än...");
+                    break;
+
+                // Startar spelet "Gissa julklappen".
+                case "3":
+                    Console.WriteLine("\nSpelet fungerar inte än...");
+                    break;
+
+                // Visa alla rim lagrade i JSON-filen.
+                case "4":
+                    // Läser in JSON-filen som en sträng.
+                    string jsonFile = File.ReadAllText("rimsamling.json");
+
+                    // Deserialiserar JSON-strängen till ett RimList-objekt (hanterar nullvärden med ett ?)
+                    RimList? rimList = JsonSerializer.Deserialize<RimList>(jsonFile);
+
+                    // Kontrollerar om värdet är null och hanterar det med ett meddelande.
+                    if (rimList == null || rimList.Rimsamling == null)
+                    {
+                        Console.WriteLine("Tyvärr, inga rim hittades.");
+                    }
+                    else
+                    {
+                        // Loopar genom listan av rim och skriver ut varje post.
+                        foreach (var item in rimList.Rimsamling)
+                        {
+                            Console.WriteLine($"Julklapp: {item.Julklapp}");
+                            Console.WriteLine($"Rim: {item.Rim}");
+                            Console.WriteLine($"Kategori: {item.Kategori}");
+
+                            // Lägger en tom rad mellan varje post.
+                            Console.WriteLine();
+                        }
+                    }
+                    break;
+
+                // Avslutar applikationen.
+                case "0":
+                    Console.WriteLine("\nAppen stängs nu ner. Lycka till med dina julklappsrim!");
+                    break;
+
+                // Visar ett felmeddelande vid ogiltigt val.
+                default:
+                    Console.WriteLine("\nEtt felaktigt val har gjorts. Försök igen!");
+                    break;
             }
-        }
+
+            // Ber användaren trycka på ENTER för att gå vidare med valet som gjorts.
+            Console.WriteLine("\nTryck på ENTER för att fortsätta.");
+            Console.ReadLine();
+
+            // Loopen avslutas när användaren väljer att avsluta med 0.
+        } while (userInput != "0");
     }
 }
